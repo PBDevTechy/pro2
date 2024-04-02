bc8a485215c04491f28409cfde2fba5e42a5398f
PBDevTechy <89288933+PBDevTechy@users.noreply.github.com>
2024-04-01 20:09:10 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 document.getElementById("button").addEventListener("click", function () {
-  // Change background to dancing rick
   document.body.style.backgroundImage = "url('download.gif')";
 
-  // Hide the heading and the button
   document.querySelector(".everything").style.display = "none";
 
-  // Play song
   var song = document.getElementById("song");
   song.play();
 });
@@ -19,8 +16,6 @@ ddocument.getElementById("myForm").addEventListener("submit", function (event) {
   var message = document.getElementById("message").value;
   var subject = document.getElementById("subject").value;
 
-  // Do something with the form data
-  // For example, you can display it on the page or send it to a server
   var formData = {
     name: name,
     email: email,
@@ -28,25 +23,11 @@ ddocument.getElementById("myForm").addEventListener("submit", function (event) {
     subject: subject,
   };
   console.log(formData);
-
-  // Optionally, you can reset the form after submission
-  // document.getElementById("myForm").reset();
 });
 
 console.log(formData);
 
-// function showAlert() {
-//   alert('This is an alert!');
-//   var videoContainer = document.getElementById('videoContainer');
-//   videoContainer.style.display = 'block';
-
-//   var myVideo = document.getElementById('myVideo');
-//   myVideo.play();
-// }
-
 function showAlert() {
   alert("Thank you! Please go to the next page...");
   window.open("https://www.youtube.com/watch?v=BbeeuzU5Qc8");
-  // Or you can use window.location.href to redirect the current window
-  // window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
 }
